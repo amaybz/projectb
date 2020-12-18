5c7f49db079e034244b22610d13e7ed2c7c01fec
maybz <amayberry87@gmail.com>
2020-12-18 15:28:25 +1100

    Update teleoptab.dart

---
+++ lib/teleoptab.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class TeleOpScreen extends StatefulWidget {
+  @override
+  _TeleOpScreenState createState() => _TeleOpScreenState();
+}
+
+class _TeleOpScreenState extends State<TeleOpScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text("Power Port"),);
+  }
+}

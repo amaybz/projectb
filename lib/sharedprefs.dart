40b1f80b66470d7f6dd68ecd524a27fdac654264
maybz <amayberry87@gmail.com>
2020-12-21 10:51:16 +1100

    Update sharedprefs.dart

--- lib/sharedprefs.dart
+++ lib/sharedprefs.dart
@@ -6,27 +6,27 @@ class MySharedPrefs {
   Future<String> readStr(String key) async {
     final prefs = await SharedPreferences.getInstance();
     final value = prefs.getString(key) ?? "";
-    print('read: $value');
+    print('read: $key Value: $value');
     return value;
   }
 
   Future<int> readInt(String key) async {
     final prefs = await SharedPreferences.getInstance();
     final value = prefs.getInt(key) ?? 0;
-    print('read: $value');
+    print('read: $key Value: $value');
     return value;
   }
 
   saveStr(String key, String data) async {
     final prefs = await SharedPreferences.getInstance();
     prefs.setString(key, data);
-    print('saved $data');
+    print('saved: $key Value: $data');
   }
 
   saveInt(String key, int data) async {
     final prefs = await SharedPreferences.getInstance();
     prefs.setInt(key, data);
-    print('saved $data');
+    print('saved: $key Value: $data');
   }
 
 }

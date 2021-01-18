ec3ec997d035cf2745407ef8cd777809654bef49
maybz <amayberry87@gmail.com>
2021-01-18 11:44:57 +1100

    Update pitscouting.dart

--- lib/pitscouting.dart
+++ lib/pitscouting.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
+import 'package:projectb/widget_counter.dart';
 import 'package:projectb/widget_headingmain.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:projectb/autotab.dart';
@@ -38,9 +39,18 @@ class _PitScoutingScreenState extends State<PitScoutingScreen> {
   bool recordSaved = false;
   //define text controllers
   final TextEditingController _txtScoutName = TextEditingController();
+  final TextEditingController _txShooting = TextEditingController();
 
   LocalTeam selectedTeam;
   List<DropdownMenuItem<String>> eventTeamsListDropDown = [];
+
+  @override
+  void initState() {
+    super.initState();
+    // Call the getJSONData() method when the app initializes
+    //_getScoringData();
+    setEventTeams(14);
+  }
 
   setEventTeams(double styleFontSize) async {
     //clear current selected event and dropdown box
@@ -177,6 +187,7 @@ class _PitScoutingScreenState extends State<PitScoutingScreen> {
   double styleFieldPadding = 5.0;
   double styleFieldPaddingSides = 10.0;
   double styleFieldScoutNameMaxWidth = 300;
+  double styleFieldtxShootingMaxWidth = 350;
   double styleFieldWidthTeam = 90;
   double styleImgFieldWidth = 90;
   double styleFontSizeBody = 15;
@@ -191,11 +202,13 @@ class _PitScoutingScreenState extends State<PitScoutingScreen> {
       styleFontSizeBody = 12;
       styleFontSizeHeadings = 16;
       styleFieldScoutNameMaxWidth = 250;
+      styleFieldtxShootingMaxWidth = 300;
     }
     if (width < 395) {
       styleFontSizeBody = 11;
       styleFontSizeHeadings = 16;
       styleFieldScoutNameMaxWidth = 200;
+      styleFieldtxShootingMaxWidth = 200;
     }
     if (width >= 600) {
       styleFontSizeBody = 15;
@@ -260,6 +273,30 @@ class _PitScoutingScreenState extends State<PitScoutingScreen> {
                             ),
                           ],
                         ),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                "Team ",
+                                style: TextStyle(fontSize: styleFontSizeBody),
+                              ),
+                              DropdownButton(
+                                value: selectedTeam == null
+                                    ? null
+                                    : selectedTeam.key,
+                                //title: "Team",
+                                items: eventTeamsListDropDown,
+                                onChanged: (item) {
+                                  setState(() {
+                                    selectedTeam = widget.eventTeams.firstWhere(
+                                        (team) => team.key == item,
+                                        orElse: () => widget.eventTeams.first);
+                                  });
+                                  print("Team Key: " + selectedTeam.key);
+                                },
+                              ),
+                            ]),
                       ]),
                 ),
               ),
@@ -285,6 +322,216 @@ class _PitScoutingScreenState extends State<PitScoutingScreen> {
                     headingText: "Robot Stats",
                     //backGroundColor: Colors.green,
                   ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        CounterWidget(
+                          value: pitData.numWeight,
+                          title: "Weight",
+                          onIncreaseStateChanged: (int value) {
+                            setState(() {
+                              pitData.numWeight++;
+                            });
+                          },
+                          onDecreaseStateChanged: (int value) {
+                            setState(() {
+                              pitData.numWeight--;
+                            });
+                          },
+                          onSetValue: (int value) {
+                            setState(() {
+                              pitData.numWeight = value;
+                            });
+                          },
+                        ),
+                        CounterWidget(
+                            value: pitData.numHeight, title: "Height",
+                          onIncreaseStateChanged: (int value) {
+                            setState(() {
+                              pitData.numHeight++;
+                            });
+                          },
+                          onDecreaseStateChanged: (int value) {
+                            setState(() {
+                              pitData.numHeight--;
+                            });
+                          },
+                          onSetValue: (int value) {
+                            setState(() {
+                              pitData.numHeight = value;
+                            });
+                          },),
+                      ]),
+
+                ]),
+              ),
+            ),
+          ),
+          FractionallySizedBox(
+            widthFactor: 0.99,
+            child: Container(
+              margin: const EdgeInsets.all(5.0),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    topRight: Radius.circular(10),
+                    bottomLeft: Radius.circular(10),
+                    bottomRight: Radius.circular(10)),
+              ),
+              child: Container(
+                padding: EdgeInsets.all(5.0),
+                child: Column(children: <Widget>[
+                  HeadingMain(
+                    styleFontSize: styleFontSizeHeadings,
+                    headingText: "Power Cells",
+                    //backGroundColor: Colors.green,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Manipulate:",
+                        style: TextStyle(fontSize: styleFontSizeBody),
+                      ),
+                      Switch(
+                        value: pitData.flCells,
+                        onChanged: (bool value) {
+                          setState(() {
+                            pitData.flCells = value;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Ground Intake:",
+                        style: TextStyle(fontSize: styleFontSizeBody),
+                      ),
+                      Switch(
+                        value: pitData.flIntakeGround,
+                        onChanged: (bool value) {
+                          setState(() {
+                            pitData.flIntakeGround = value;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Loading Station Intake:",
+                        style: TextStyle(fontSize: styleFontSizeBody),
+                      ),
+                      Switch(
+                        value: pitData.flIntakeHigh,
+                        onChanged: (bool value) {
+                          setState(() {
+                            pitData.flIntakeHigh = value;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Target (Lower):",
+                        style: TextStyle(fontSize: styleFontSizeBody),
+                      ),
+                      Switch(
+                        value: pitData.flTargetLow,
+                        onChanged: (bool value) {
+                          setState(() {
+                            pitData.flTargetLow = value;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Target (Outer):",
+                        style: TextStyle(fontSize: styleFontSizeBody),
+                      ),
+                      Switch(
+                        value: pitData.flTargetOuter,
+                        onChanged: (bool value) {
+                          setState(() {
+                            pitData.flTargetOuter = value;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Target (Inner):",
+                        style: TextStyle(fontSize: styleFontSizeBody),
+                      ),
+                      Switch(
+                        value: pitData.flTargetInner,
+                        onChanged: (bool value) {
+                          setState(() {
+                            pitData.flTargetInner = value;
+                          });
+                        },
+                      ),
+                    ],
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        CounterWidget(
+                          value: pitData.numStorage,
+                          title: "Storage Capacity",
+                          onIncreaseStateChanged: (int value) {
+                            setState(() {
+                              pitData.numStorage++;
+                            });
+                          },
+                          onDecreaseStateChanged: (int value) {
+                            setState(() {
+                              pitData.numStorage--;
+                            });
+                          },
+                          onSetValue: (int value) {
+                            setState(() {
+                              pitData.numStorage = value;
+                            });
+                          },
+                        ),
+                      ]),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Shooting Mechanism: ",
+                        style: TextStyle(fontSize: styleFontSizeBody),
+                      ),
+                      ConstrainedBox(
+                        constraints: BoxConstraints(
+                            maxWidth: styleFieldtxShootingMaxWidth),
+                        child: TextField(
+                          controller: _txShooting,
+                          decoration:
+                          InputDecoration(hintText: 'Cannon, hooded shooter, twin flywheel etc'),
+                        ),
+                      ),
+                    ],
+                  ),
+
                 ]),
               ),
             ),

cf5dea9742d72c06077c1d0b4e95eec450e67297
Aiden Mayberry <amayberry87@gmail.com>
2022-04-17 21:09:08 +1000

    set a max width for teams

--- lib/main.dart
+++ lib/main.dart
@@ -40,13 +40,6 @@ class MyApp extends StatelessWidget {
     return GestureDetector(
       onTap: () {
         FocusScopeNode currentFocus = FocusScope.of(context);
-
-        //if (!currentFocus.hasPrimaryFocus) {
-        //  currentFocus.unfocus();
-        //}
-
-        //if (!currentFocus.hasPrimaryFocus && currentFocus.focusedChild != null) { currentFocus.focusedChild!.unfocus(); } },
-
         if (!currentFocus.hasPrimaryFocus &&
             currentFocus.focusedChild != null) {
           FocusManager.instance.primaryFocus!.unfocus();
